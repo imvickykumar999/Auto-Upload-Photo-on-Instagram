fab74e7395a3c0e257f09e06cd145db1fc712a6a
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-27 22:15:17 +0530

    error resolved

--- Executable OpenAI/Daily_Upload.py
+++ Executable OpenAI/Daily_Upload.py
@@ -28,14 +28,36 @@ API_Key = getpass.getpass('Enter API key : ')
 openai.api_key = API_Key
 
 print('\nUploading ...')
-models = openai.Model.list()
-
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
                 messages=[{"role": "user", 
                            "content": "Trending Minecraft Games"}])
 
 topics = completion.choices[0].message.content
-topic = random.choice(topics.split('\n')[2:-2]).split('. ')[1]
+
+try:
+    topic = random.choice(topics.split('\n')[2:-2]).split('. ')[1]
+except:
+    topics = '''1. Minecraft 1.17: Caves and Cliffs Update - This upcoming update to Minecraft is highly anticipated by players. It introduces new underground features, biomes, mobs, and blocks, enhancing the exploration and mining experience.
+
+2. Minecraft Dungeons - This spin-off game combines the Minecraft universe with action-packed dungeon crawling gameplay. Players can team up with friends to defeat mobs and bosses, collect gear, and explore various levels.
+
+3. Minecraft Earth - A mobile augmented reality game that allows players to build and explore Minecraft creations in the real world. It incorporates real-world locations and encourages collaboration with other players.
+
+4. Minecraft: Story Mode - Developed by Telltale Games, this episodic adventure game takes players on an interactive story-driven journey set in the Minecraft universe. Players make choices that affect the outcome and can shape the narrative.
+
+5. Minecraft Education Edition - Designed for educational purposes, this version of Minecraft provides a platform for teachers to engage students in learning activities. It offers various features, including lesson plans, collaborative building, and coding exercises.
+
+6. Minecraft Skyblock - This popular Minecraft game mode challenges players to survive on a floating island with limited resources. They must use their creativity and problem-solving skills to expand their island, gather resources, and survive.
+
+7. Minecraft Bed Wars - A multiplayer game mode where players compete to destroy each other's beds while defending their own. It requires strategic planning, teamwork, and quick reflexes to outsmart opponents and emerge victorious.
+
+8. Minecraft Survival Games - Inspired by the "Hunger Games" series, this game mode throws players into a map where they must scavenge for weapons and armor, eliminate opponents, and be the last one standing.
+
+9. Minecraft Hypixel - Hypixel is a popular Minecraft server that offers a wide range of custom mini-games, including Bed Wars, Skyblock, and Survival Games. It attracts a large player base due to its diverse and engaging gameplay options.
+
+10. Minecraft Creative Mode - Although not new, Minecraft's Creative Mode remains a favorite among players. It allows them unlimited resources, the ability to fly, and the freedom to create and build whatever they imagine in the game.
+'''
+    topic = random.choice(topics.split('\n')[2:-2]).split('. ')[1]
 
 n=1
 image_resp = openai.Image.create(prompt=topic, n=n, size="512x512")
